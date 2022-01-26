eec090351f2fbbfede1282ac2c62aaee6fece0ea
VadimIgnatov <vignatov@my.yorku.ca>
2022-01-25 21:14:42 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import skyline from './city-skyline.jpg';
+import tower from './cn-tower.jpg';
+import newcityhall from './new-city-hall.jpg';
+import oldcityhall from './old-city-hall.jpg';
+import rogers from './rogers-center.jpg';
+import rom from './rom.jpg';
+
 
 function App() {
-  const images = [logo, logo, logo, logo, logo, logo];
+  const images = [skyline, oldcityhall, tower,  rom, rogers, newcityhall];
   const [selectedImage, setSelectedImage] = useState(null);
 
   return (
