3be969dfe63f28a8fef8618766c312015f3f69e1
VadimIgnatov <vignatov@my.yorku.ca>
2022-01-31 23:53:36 -0500

    edit2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import newcityhall from './new-city-hall.jpg';
 import oldcityhall from './old-city-hall.jpg';
 import rogers from './rogers-center.jpg';
 import rom from './rom.jpg';
+import fb from './fb.png';
+import insta from './insta.png';
+import linked from './linked.png';
+import twitter from './twitter.png';
 
 
 function App() {
@@ -26,10 +30,16 @@ function App() {
         <img src={oldcityhall} id= 'b'></img>
         <img src={tower} id= 'c'></img>
         <img src={rom} id= 'd'></img>
-        <img src={newcityhall} id= 'e'></img>
-        <img src={rogers} id= 'f'></img>
-        
+        <img src={rogers} id= 'e'></img>
+        <img src={newcityhall} id= 'f'></img>
       </section>
+      <section id="social">
+
+        <a href="https://twitter.com/cityoftoronto?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><img src={twitter}></img></a>
+        <a href="https://www.facebook.com/cityofto/" target="_blank"><img src={fb}></img></a>
+        <a href="https://ca.linkedin.com/company/city-of-toronto" target="_blank"><img src={linked}></img></a>
+        <a href="https://www.instagram.com/cityofto/?hl=en" target="_blank"><img src={insta}></img></a>
+       </section> 
       {/* <section>
         {
           images.map(image => <img src={image} alt="logo" onClick={()=>setSelectedImage(image)} />)
